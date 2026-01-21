2a370c215757e160d36ca2223aeb0c046c38506f
Utkarshpatg <utkarshpathak5202005@gmail.com>
2026-01-21 15:08:02 +0530

    delete successfully

--- NodeJs-Programming/Nodejs_Programming/Q-2/index.js
+++
@@ -1,5 +0,0 @@
-const { capitalize, reverseString, countVowels } = require("./stringUtils");
-
-console.log(capitalize("hello"));      
-console.log(reverseString("hello"));   
-console.log(countVowels("hello"));  
